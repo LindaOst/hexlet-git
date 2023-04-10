ed078a5f1a30b8d1bd64441eee3cce38049c0109
LindaOst <120901085+LindaOst@users.noreply.github.com>
2023-04-10 17:36:30 +0300

    Update translate.py

--- IBM/translate.py
+++ IBM/translate.py
@@ -1,7 +1,7 @@
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-# Замените значения YOUR_APIKEY и YOUR_URL на ваши учетные данные
+
 api_key = '3HLPjtdJ6b7j-m6KMJ5ja9V1Vzye4EFheWzlBfAwygTQ'
 url = 'https://api.us-east.language-translator.watson.cloud.ibm.com/instances/cfcfe476-a522-43cb-845b-797d57825676'
 
